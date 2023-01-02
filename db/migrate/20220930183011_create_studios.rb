4ba69a6750c1f25808b8d48e76e507f28f92a909
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-02 09:16:43 -0700

    test: create tests

--- db/migrate/20220930183011_create_studios.rb
+++ db/migrate/20220930183011_create_studios.rb
@@ -3,6 +3,8 @@ class CreateStudios < ActiveRecord::Migration[5.2]
     create_table :studios do |t|
       t.string :name
       t.string :location
+
+      t.timestamps
     end
   end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Movie do
+RSpec.describe Actor, type: :model do
   describe 'relationships' do
-    it { should belong_to :studio }
     it { should have_many :actor_movies }
-    it { should have_many :actors }
+    it { should have_many :movies }
   end
 end
