ee38308edfc3e9b2e60c07d2c8be51a266d98e32
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-02 09:54:56 -0700

    test: studio index movie list

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'the studio index' do
   before(:each) do
     @studio_1 = FactoryBot.create(:studio)
     @studio_2 = FactoryBot.create(:studio)
+
+    @movie_1 = FactoryBot.create(:movie, studio: @studio_1)
+    @movie_2 = FactoryBot.create(:movie, studio: @studio_1)
+    @movie_3 = FactoryBot.create(:movie, studio: @studio_2)
   end
 
   describe 'When I visit the studio index page' do
@@ -14,6 +18,16 @@ RSpec.describe 'the studio index' do
       expect(page).to have_content(@studio_1.location)
       expect(page).to have_content(@studio_2.name)
       expect(page).to have_content(@studio_2.location)
+    end
+
+    it 'shows all of the studios including name and location' do
+      visit "/studios"
+
+      within(:xpath, '//div[@id="studio-1-movies"]') do
+        expect(page).to have_content(@movie_1.name)
+        expect(page).to have_content(@movie_2.name)
+        expect(page).to_not have_content(@movie_3.name)
+      end
     end
   end
 end
